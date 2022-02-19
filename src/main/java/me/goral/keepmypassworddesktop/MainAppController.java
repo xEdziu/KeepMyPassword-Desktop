018c2eac713fd817ffa987f5ea05be36212e07b5
Adrian Goral <adrian.goral@gmail.com>
2022-02-20 00:38:11 +0100

    Initial look on welcome app

--- src/main/java/me/goral/keepmypassworddesktop/MainAppController.java
+++ src/main/java/me/goral/keepmypassworddesktop/MainAppController.java
@@ -1,9 +1,17 @@
 package me.goral.keepmypassworddesktop;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MainAppController {
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    protected void onLoginButtonClick() {
+        
+    }
 //    @FXML
 //    private Label welcomeText;
 //
