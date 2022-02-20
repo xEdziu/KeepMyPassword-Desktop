9775cbbd5800ddc762979fe24ef63ba7bda2eeb1
Adrian Goral <adrian.goral@gmail.com>
2022-02-20 20:58:20 +0100

    Started working on database connection

---
+++ src/main/java/me/goral/keepmypassworddesktop/database/DatabaseHandler.java
@@ -0,0 +1,27 @@
+package me.goral.keepmypassworddesktop.database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    public static void connect(){
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:database.db";
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection established");
+        } catch (SQLException e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null){
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,2 @@
-module me.goral.keepmypassworddesktop {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-
-    opens me.goral.keepmypassworddesktop to javafx.fxml;
-    exports me.goral.keepmypassworddesktop;
+module $MODULE_NAME$ {
 }
