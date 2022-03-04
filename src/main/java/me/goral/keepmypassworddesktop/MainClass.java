d113ead0ac170ace8c05f2c3145f0f50b9502882
Adrian Goral <adrian.goral@gmail.com>
2022-03-04 20:45:12 +0100

    Added maven assembly plugin

---
+++ src/main/java/me/goral/keepmypassworddesktop/MainClass.java
@@ -0,0 +1,7 @@
+package me.goral.keepmypassworddesktop;
+
+public class MainClass {
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,6 @@ module me.goral.keepmypassworddesktop {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
