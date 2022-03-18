5aeeab99a698efcd100596f28a578255e37e3de0
Adrian Goral <adrian.goral@gmail.com>
2022-03-18 15:08:03 +0100

    Added documentation of SHAUtil

--- src/main/java/me/goral/keepmypassworddesktop/util/SHAUtil.java
+++ src/main/java/me/goral/keepmypassworddesktop/util/SHAUtil.java
@@ -5,6 +5,12 @@ import org.bouncycastle.util.encoders.Hex;
 
 public class SHAUtil {
 
+    /**
+     * It hashes the input string using SHA3-512.
+     * 
+     * @param input The input string to hash.
+     * @return The SHA-512 hash of the input string.
+     */
     public static String hashSHA(String input){
         SHA3.DigestSHA3 digestSHA3 = new SHA3.Digest512();
         byte[] digest = digestSHA3.digest(input.getBytes());
