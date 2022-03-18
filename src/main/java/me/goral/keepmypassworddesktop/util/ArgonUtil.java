0939d4facd49f8b6b2041f25e2b896e54da4c01b
Adrian Goral <adrian.goral@gmail.com>
2022-03-18 14:58:31 +0100

    Added documentation for ArgonUtil

--- src/main/java/me/goral/keepmypassworddesktop/util/ArgonUtil.java
+++ src/main/java/me/goral/keepmypassworddesktop/util/ArgonUtil.java
@@ -10,12 +10,24 @@ public class ArgonUtil {
 
     static Argon2Advanced argon2 = Argon2Factory.createAdvanced(Argon2Factory.Argon2Types.ARGON2id);
 
+    /**
+     * It hashes the password using the Argon2 algorithm.
+     * 
+     * @param password The password to be hashed.
+     * @param salt A random salt.
+     * @return The hash of the password.
+     */
     public static String encrypt(String password, String salt){
         char[] pwd = password.toCharArray();
         byte[] s = Base64.getDecoder().decode(salt);
         return argon2.hash(22, 65536, 1, pwd, Charset.defaultCharset(), s);
     }
 
+    /**
+     * Generate a 128-bit salt
+     * 
+     * @return A 128-byte random salt.
+     */
     public static byte[] generateSalt(){
         byte[] s = new byte[128];
         new SecureRandom().nextBytes(s);
