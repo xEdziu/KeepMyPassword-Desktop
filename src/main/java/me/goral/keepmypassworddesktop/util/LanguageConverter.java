526a81c75d3e444a983edb66fe9be434c5586609
Adrian Goral <adrian.goral@gmail.com>
2022-09-29 18:12:11 +0200

    Added functions that replace locales and languages

--- src/main/java/me/goral/keepmypassworddesktop/util/LanguageConverter.java
+++ src/main/java/me/goral/keepmypassworddesktop/util/LanguageConverter.java
@@ -1,6 +1,8 @@
 package me.goral.keepmypassworddesktop.util;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class LanguageConverter {
 
@@ -21,8 +23,21 @@ public class LanguageConverter {
         this.langs.put("ar-jo","Jordan");//NON-NLS
         this.langs.put("ar-kw","Kuwait");//NON-NLS
         this.langs.put("ar-lb","Lebanon");//NON-NLS
-        this.langs.put("ar-ly","Libya ");//NON-NLS
+        this.langs.put("ar-ly","Libya");//NON-NLS
         this.langs.put("ar-ma","Morocco");//NON-NLS
         this.langs.put("sq-AL","Oman");//NON-NLS
+    }
+
+    public String convertToLanguage(String locale){
+        return this.langs.get(locale);
+    }
+
+    public String convertToLocale(String language){
+        for (Map.Entry<String, String> entry : this.langs.entrySet()){
+            if (Objects.equals(entry.getValue(), language)){
+                return entry.getKey();
+            }
+        }
+        return null;
     }
 }
